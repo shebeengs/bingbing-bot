fb60734047d452493161852d29176eceba2b24f1
shebeengs <54754746+shebeengs@users.noreply.github.com>
2021-02-27 07:27:02 +0800

    Update weeklymeeting.js

--- commands/weeklymeeting.js
+++ commands/weeklymeeting.js
@@ -8,13 +8,13 @@ module.exports = {
     execute(bot,myChannel,servethCh){
         setInterval(function () {
             var dateToday = new Date();
-            var meetStart = (dateToday.getDay() === 6) && (dateToday.getHours() === 23) && (dateToday.getMinutes() === 25) && (dateToday.getSeconds() === 00);
+            var meetStart = (dateToday.getDay() === 4) && (dateToday.getHours() === 9) && (dateToday.getMinutes() === 00) && (dateToday.getSeconds() === 00);
             var meet1hour = (dateToday.getDay() === 4) && (dateToday.getHours() === 8) && (dateToday.getMinutes() === 30) && (dateToday.getSeconds() === 00);
             var meet5min = (dateToday.getDay() === 4) && (dateToday.getHours() === 8) && (dateToday.getMinutes() === 55) && (dateToday.getSeconds() === 00);
 
             if (meetStart) {
                 bot.channels.cache.get(myChannel).send("@everyone Our weekly meeting is starting! Join voice channel now.");
-                //bot.channels.cache.get(servethCh).send("@everyone Our weekly meeting is starting! Join voice channel now.");
+                bot.channels.cache.get(servethCh).send("@everyone Our weekly meeting is starting! Join voice channel now.");
             }
             else if (meet1hour) {
                 bot.channels.cache.get(myChannel).send("Weekly meeting in 30 minutes!");
@@ -24,6 +24,7 @@ module.exports = {
                 bot.channels.cache.get(myChannel).send("@everyone Weekly meeting in 5 minutes!");
                 bot.channels.cache.get(servethCh).send("@everyone Weekly meeting in 5 minutes!");
             }
+            console.log(dateToday);
         }, 999);
     }
 }
