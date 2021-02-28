e2b01348cb0227a36ee469e536d34d2911d71e95
shebeengs <54754746+shebeengs@users.noreply.github.com>
2021-02-28 22:07:14 +0800

    fix date

--- commands/list.js
+++ commands/list.js
@@ -1,19 +1,7 @@
-const moment= require('moment');
-
 module.exports = {
     name: 'list',
     description: 'Allows user to display a list of commands',
     execute(msg){
-        msg.channel.send('```\nLIST OF COMMANDS for Botbotbot \n\n!list \n!login \n!logout \n!bio - sets a 5-min timer \n!break - sets a 10-min timer \n💩 - sets a 15-min timer \n!lunch - sets a 60-min timer; reminds after 55 mins \n!timer <number of minutes> - sets a custom timer. Example: !break 30 (sets a 30-min timer) \n!back \n\nOTHERS: \n• Reacts 👋 to "hello" and "hi"\n• Weekly Meeting (THU 17:00) reminders: 16:30, 16:55 and 17:00 ```');
+        msg.channel.send('```\nLIST OF COMMANDS for Botbotbot \n\n!list \n!login \n!logout \n!bio - sets a 5-min timer \n!break - sets a 10-min timer \n💩 - sets a 15-min timer \n!lunch - sets a 60-min timer; reminds after 55 mins \n!timer <number of minutes> - sets a custom timer. Example: !break 30 (sets a 30-min timer) \n!back \n\nOTHERS: \n• Reacts 👋 to "hello" and "hi"\n• Weekly Meeting (THU 17:00) reminders: 16:30, 16:55 and 17:00 \nPayday reminder: 1st and 15th of the month```');
     }
 }
-
-//\n\nON-GOING: \n• Payroll reminders: 1st and EOM \n• Shift start/end reminders \n• Lunch start/end reminder \n• and more 😁
-
-/* 
-Payroll remiders
-Shift start/end
-Lunch start/end
-Version updates?
-
- */
