16270ea6de4f61825649c960afa2f32a6aa4e581
shebeengs <54754746+shebeengs@users.noreply.github.com>
2021-02-28 09:33:31 +0800

    fix time

--- events/payroll.js
+++ events/payroll.js
@@ -8,9 +8,9 @@ module.exports = {
         setInterval(function () {
             var dateToday = new Date();
             var nextMonth = new Date(new Date(dateToday).setMonth(dateToday.getMonth()));
-            var every15 = (dateToday.getDate() === 15) && (dateToday.getHours() === 2) && (dateToday.getMinutes() === 00) && (dateToday.getSeconds() === 00);
+            var every15 = (dateToday.getDate() === 15) && (dateToday.getHours() === 10) && (dateToday.getMinutes() === 00) && (dateToday.getSeconds() === 00);
             
-            var everyEOM = result && (dateToday.getHours() === 2) && (dateToday.getMinutes() === 00) && (dateToday.getSeconds() === 00);
+            var everyEOM = result && (dateToday.getHours() === 10) && (dateToday.getMinutes() === 00) && (dateToday.getSeconds() === 00);
             isLastDay(dateToday);
 
             if (every15 || everyEOM) {
